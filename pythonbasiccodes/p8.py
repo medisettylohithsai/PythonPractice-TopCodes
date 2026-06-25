1bfd37260cd5ed4834f56ec7c7706a6c69ba1739
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-25 07:21:58 +0530

    Add files via upload

---
+++ pythonbasiccodes/p8.py
@@ -0,0 +1,6 @@
+#Write a program that checks if a number is divisible by both 5 and 7.
+n=int(input())
+if n%5==0 and n%7==0:
+    print("num divisible by both 5 and 7")
+else:
+    print("num not divisible by both 5 and 7")
