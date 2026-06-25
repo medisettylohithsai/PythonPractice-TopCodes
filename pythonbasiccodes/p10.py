0a2f8ae5ef2e4935f8459289d998c856d0d985b4
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-25 07:22:42 +0530

    Add files via upload

---
+++ pythonbasiccodes/p10.py
@@ -0,0 +1,5 @@
+# Create a program that uses different data types (int, float, string) in one statement.
+a=int(input())
+b=float(input())
+c=str(input())
+print(f"integer: {a} ,float: {b},string:{c}")
