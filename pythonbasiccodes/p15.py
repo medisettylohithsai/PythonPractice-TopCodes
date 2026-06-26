242cbf9779cf5d11c18ac5a72a965d68acdb6ba4
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-26 07:18:10 +0530

    Add files via upload

---
+++ pythonbasiccodes/p15.py
@@ -0,0 +1,8 @@
+#Create a program that checks if a given number is positive, negative, or zero.
+n=int(input())
+if n>0:
+    print("+ number")
+elif n<0:
+    print("- number")
+else:
+    print("0")
