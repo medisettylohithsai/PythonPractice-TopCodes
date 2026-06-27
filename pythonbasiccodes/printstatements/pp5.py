01c7f962ba34bae580ee677b3845f18366de4c79
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-27 07:32:04 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp5.py
@@ -0,0 +1,6 @@
+""" **Escape Characters:**
+   - Create a program that uses escape characters to print a multi-line string.
+"""
+# Escape Characters: Multi-line String
+
+print("Welcome to Python\nThis is a multi-line string\nUsing escape characters")
