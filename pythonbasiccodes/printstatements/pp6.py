913fff3b1e6b86aa28aaa1cdd087c633cc14ae11
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-27 07:32:20 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp6.py
@@ -0,0 +1,9 @@
+""". **Print without Line Break:**
+   - Write a program that prints multiple items on the same line without line breaks.
+"""
+# Print without Line Break
+
+print("Python", end=" ")
+print("is", end=" ")
+print("easy", end=" ")
+print("to learn")
