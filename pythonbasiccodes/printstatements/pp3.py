c0bb0b760be182b7d108e7a7366bf68a4775b044
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-27 07:31:24 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp3.py
@@ -0,0 +1,12 @@
+""". **Formatted Output:**
+   - Write a program that uses formatted strings to print variables with descriptive messages.
+"""
+# Formatted Output using f-strings
+
+name = "Lohith"
+age = 21
+course = "B.Tech"
+
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"I am studying {course}.")
