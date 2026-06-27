1fa2656bdf005b45f0bcdcea6b82561f02a7e466
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-27 07:33:51 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp10.py
@@ -0,0 +1,6 @@
+"""**Print with Separator:**
+    - Develop a program that prints multiple items separated by a specific character.
+"""
+# Print with Separator
+
+print("Python", "Java", "C++", sep=";")
