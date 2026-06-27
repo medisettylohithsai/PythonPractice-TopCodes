9b9f5fdce32cc3e129a58b52631fbd68378ed476
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-27 07:32:39 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp7.py
@@ -0,0 +1,9 @@
+"""**Variable Printing:**
+   - Develop a program that takes user input and prints a personalized greeting.
+"""
+# Variable Printing: Personalized Greeting
+
+name = input("Enter your name: ")
+
+print("Hello,", name + "!")
+print("Welcome to Python Programming.")
