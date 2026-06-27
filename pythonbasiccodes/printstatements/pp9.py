b435d86c6bafd893bc0ad44204c9c8b69eea257d
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-27 07:33:29 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp9.py
@@ -0,0 +1,9 @@
+"""**Precision Printing:**
+   - Write a program that uses formatted strings to print a floating-point number with two decimal places.
+"""
+
+    # Precision Printing: Display a floating-point number with two decimal places
+
+number = 3.14159265
+
+print(f"The value is: {number:.2f}")
