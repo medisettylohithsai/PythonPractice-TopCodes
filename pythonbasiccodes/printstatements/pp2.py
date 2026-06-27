bdaf0f03cdffa10852f3376d8ddc12f24c7682ef
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-27 07:31:03 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp2.py
@@ -0,0 +1,8 @@
+"""**Multiple Prints:**
+   - Create a program that uses multiple `print` statements to output a message with line breaks.
+"""
+# Multiple Prints: Output a message with line breaks
+
+print("Hello!")
+print("Welcome to Python Programming.")
+print("Have a great day!")
