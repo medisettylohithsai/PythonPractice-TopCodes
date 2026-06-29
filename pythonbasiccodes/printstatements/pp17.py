d6fe0767b553d72bc52fc1804fae75b63288e71f
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-29 10:04:12 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp17.py
@@ -0,0 +1,8 @@
+"""**Formatted Output with Arithmetic:**
+    - Create a program that uses formatted strings to print the result of an arithmetic expression.
+"""
+
+a = 10
+b = 5
+
+print(f"The sum of {a} and {b} is {a + b}")
