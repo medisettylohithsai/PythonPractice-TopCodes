d92edc78f29420e7487ee5a35acce49a0e9fb65b
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-29 10:02:17 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp12.py
@@ -0,0 +1,8 @@
+"""**Print with End Parameter:**
+    - Write a program that prints multiple items with a custom `end` parameter.
+
+"""
+
+print("Python", end=" - ")
+print("Java", end=" - ")
+print("C++")
