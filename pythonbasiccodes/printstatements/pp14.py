b096377d55f51a193cc861e58c6d764dd9db3e42
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-29 10:03:03 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp14.py
@@ -0,0 +1,8 @@
+"""**Print with Triple Quotes:**
+    - Create a program that uses triple-quoted strings to print a multi-line message.
+"""
+message = """Welcome to Python Programming
+This is a multi-line message
+Using triple quotes."""
+
+print(message)
