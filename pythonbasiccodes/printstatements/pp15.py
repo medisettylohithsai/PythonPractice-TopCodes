f1fc8f382e9a05f1e58acc9a786c4f10a429b466
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-29 10:03:25 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp15.py
@@ -0,0 +1,9 @@
+"""**Printing Special Characters:**
+    - Write a program that prints special characters (e.g., copyright symbol, newline) using Unicode escape sequences.
+
+"""
+
+print("Copyright Symbol: \u00A9")
+print("Registered Symbol: \u00AE")
+print("Trademark Symbol: \u2122")
+print("Hello\nWorld")
