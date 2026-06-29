1b254823e9cf0ace316e3f9a160d29ffd554d3f7
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-29 10:05:26 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp20.py
@@ -0,0 +1,9 @@
+""". **Print with f-strings:**
+- Write a program that uses f-strings to print variables and expressions.
+"""
+
+name = "Lohith"
+age = 21
+
+print(f"My name is {name} and I am {age} years old.")
+print(f"Next year I will be {age + 1} years old.")
