e386583e5499225feea416584982800b95663300
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-29 10:03:48 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp16.py
@@ -0,0 +1,5 @@
+"""**Print with Separator and End:**
+    - Develop a program that uses both the `sep` and `end` parameters in the `print` function.
+"""
+print("Python", "Java", "C++", sep=" | ", end=" : ")
+print("Programming Languages")
