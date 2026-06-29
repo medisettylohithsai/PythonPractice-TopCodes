936b4fc1611270bada42a27134a43ad7daa10a82
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-29 10:05:05 +0530

    Add files via upload

---
+++ pythonbasiccodes/printstatements/pp19.py
@@ -0,0 +1,10 @@
+""". **Conditional Printing:**
+    - Develop a program that uses the `print` function inside a conditional statement.
+"""
+
+num = int(input())
+
+if num > 0:
+    print("Positive Number")
+else:
+    print("Negative Number or Zero")
