a4c7bd136bbcdfa258edf372a110e20eac09b175
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-30 09:01:13 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/SquareRoot.py
@@ -0,0 +1,13 @@
+# """. **Square Root:**
+#    - Write a program that calculates the square root of a given number.
+# """
+# num = float(input("Enter a number: "))
+
+# square_root = num ** 0.5
+
+# print("Square root =", square_root)
+
+import math
+n=float(input("Enter the number"))
+s=math.sqrt(n)
+print(s)
