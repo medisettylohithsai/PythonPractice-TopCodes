d923d4c3f5464554682e1cec69cdecde59163874
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-30 08:59:48 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/Mul.py
@@ -0,0 +1,10 @@
+"""**Multiplication:**
+   - Write a program that multiplies two numbers and prints the result.
+"""
+
+def Mul(a,b):
+    x=a*b 
+    print(x)
+a=int(input())
+b=int(input())
+Mul(a,b)
