3f9d6f4862b0445d5712f71f6ada524ee2822c02
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-30 08:58:35 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/ceil&floor.py
@@ -0,0 +1,9 @@
+"""**Ceiling and Floor:**
+    - Develop a program that uses the `math` module to find the ceiling and floor of a given number.
+"""
+import math
+
+num = float(input())
+
+print("Ceiling:", math.ceil(num))
+print("Floor:", math.floor(num))
