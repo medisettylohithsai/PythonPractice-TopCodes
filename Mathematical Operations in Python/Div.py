283451ca3c0dd4b4af824a5460f19e936ab125fb
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-30 08:59:01 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/Div.py
@@ -0,0 +1,11 @@
+""" **Division:**
+   - Develop a program that divides one number by another and prints the result.
+"""
+
+
+def div(a,b):
+    x=a/b 
+    print(x)
+a=int(input())
+b=int(input())
+div(a,b)
