1b3ecffc2bb464f9782d19f48d592c9108c14838
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-30 09:01:36 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/sub.py
@@ -0,0 +1,10 @@
+"""**Subtraction:**
+   - Create a program that subtracts one number from another and prints the result.
+"""
+
+def Sub(a,b):
+    x=a-b 
+    return x
+a=int(input())
+b=int(input())
+print(Sub(a,b))
