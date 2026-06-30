db2d6d53d0c36ab49416e9973efa115379a52abd
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-06-30 08:59:26 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/Modulus.py
@@ -0,0 +1,9 @@
+"""**Modulus:**
+   - Develop a program that finds the remainder when one number is divided by another.
+"""
+def mod(a,b):
+    x=a%b 
+    print(x)
+a=int(input())
+b=int(input())
+mod(a,b)
