5f422f561201b9e8e4c341347820d6a0a785527d
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-07-01 09:54:41 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/avg.py
@@ -0,0 +1,11 @@
+""" **Average of Three Numbers:**
+    - Write a program that calculates the average of three numbers.
+"""
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+average = (a + b + c) / 3
+
+print(f"Average = {average}")
