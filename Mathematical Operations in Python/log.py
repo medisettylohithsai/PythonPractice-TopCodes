9748c2b36e1b6cb2be1880f87156a3d3474758b5
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-07-01 09:55:13 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/log.py
@@ -0,0 +1,10 @@
+"""Logarithm:**
+    - Develop a program that uses the `math` module to find the natural logarithm of a number.
+"""
+import math
+
+num = float(input("Enter a positive number: "))
+
+result = math.log(num)
+
+print("Natural Logarithm =", result)
