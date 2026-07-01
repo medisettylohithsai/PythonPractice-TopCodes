3ffe3ab44b8e7a3edbf40bf3120bc5dfe5bed0c0
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-07-01 09:54:58 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/Exponentialfunction.py
@@ -0,0 +1,10 @@
+"""Exponential Function:**
+    - Create a program that calculates the exponential function of a given number.
+"""
+import math
+
+num = float(input("Enter a number: "))
+
+result = math.exp(num)
+
+print("Exponential value =", result)
