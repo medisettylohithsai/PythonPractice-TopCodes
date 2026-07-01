4f5d40032e1aef0f2b83b69d207acc562b6c7ff9
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-07-01 09:54:22 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/areaofcircle.py
@@ -0,0 +1,7 @@
+"""**Area of a Circle:**
+    - Create a program that calculates the area of a circle using the formula A = πr².
+"""
+r=float(input())
+
+a=3.14*r*r
+print(f"area of circle is:{a}")
