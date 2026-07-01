88fa5f65530a09e99b797db7fcdecf8e155b1bee
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-07-01 09:57:09 +0530

    Add files via upload

---
+++ Mathematical Operations in Python/volofsphere.py
@@ -0,0 +1,11 @@
+""". **Volume of a Sphere:**
+    - Develop a program that calculates the volume of a sphere using the formula V = (4/3)πr³.
+"""
+
+import math
+
+r = float(input("Enter the radius: "))
+
+v = (4/3) * math.pi * r**3
+
+print(f"Volume of sphere is: {v}")
