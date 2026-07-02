1cbf0f48416ef8ac0ace9941c144b55add221379
LohithSaiMedisetty <157821306+medisettylohithsai@users.noreply.github.com>
2026-07-02 10:16:33 +0530

    Add files via upload

---
+++ Conditional and Control Flow (if statements, loops)/factorial.py
@@ -0,0 +1,9 @@
+#Create a program that uses a loop to print the factorial of a given number.
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    return fact
+
+num = int(input("Enter a number: "))
+print("Factorial =", factorial(num))
